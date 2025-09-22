b8c960a8e512859c4ba13a771b332d79c1652852
TheKallux <kalle.lindberg@iths.se>
2025-09-22 10:15:09 +0200

    Add project files.

---
+++ LABB1/Program.cs
@@ -0,0 +1,14 @@
+﻿string input = "29535123p48723487597645723645";
+int firstLetter = input[0];
+
+for (int i = 1; i < input.Length; i++)
+{
+    if (i == input[0])
+    {
+        break;
+    }
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write(input[i]);
+}
+
+Console.ReadLine();
