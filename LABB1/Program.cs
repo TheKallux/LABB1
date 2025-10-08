77dd04a7f2e4633debf9c945490af7bb27e42fa9
TheKallux <kallelindberg.jobb@gmail.com>
2025-10-08 10:48:24 +0200

    Fixed the code where a matching number pair would not move the loop forward to the next number pair, but create another substring from the same starting digit.

--- LABB1/Program.cs
+++ LABB1/Program.cs
@@ -5,35 +5,36 @@ namespace LABB1
 
     class Program
     {
-        static void Main(string[] args) //----------------------------------------------------------------------------- MAIN
+        static void Main(string[] args) 
         {
             Console.WriteLine("Mata in en text med siffror och nummer.");
             string input = Console.ReadLine();
+            Console.Clear();
 
             long subsAdd = 0;
 
-            for (int i = 0; i < input.Length; i++) // Börja loopa igenom strängen från index 0 kör till slutet av strängen.
+            for (int i = 0; i < input.Length; i++) 
             {
-                if (!char.IsNumber(input[i])) // Kollar om tecknet på index 0 är en siffra. Om != siffra, hoppa över.
+                if (!char.IsNumber(input[i])) 
                 {
                     continue;
                 }
 
-                for (int j = i + 1; j < input.Length; j++) // j söker efter siffra som matchar i, börjar på index 1 (alltid en siffra framåt) och slutar på sista index (om inget annat sägs).
+                for (int j = i + 1; j < input.Length; j++) 
                 {
-                    if (!char.IsNumber(input[j])) // Samma check som i-loopen, men om j hittar tecken som ej är siffra, hoppa ur loop (eftersom strängar med bokstäver ska ignoreras).
+                    if (!char.IsNumber(input[j])) 
                     {
                         break;
                     }
 
-                    if (input[i] != input[j]) // Fortsätt loopen så länge vi inte hittar en matchning till i.
+                    if (input[i] != input[j]) 
                     {
                         continue;
                     }
 
-                    string rangeSub = input[i..(j + 1)]; // Vid match, skär ut sub-sträng från i till j + 1 (+ 1 eftersom [i] är tecknet och j är index, alltså -1) som börjar på index i (0 för första substringen). 
+                    string rangeSub = input[i..(j + 1)]; 
                     
-                    for (int k = 0; k < input.Length; k++) // Loopa igenom input.Legnth. Om k befinner sig mellan i och j, inkluderade, skriv ut substräng i blått. Om inte, skriv ut i vitt.
+                    for (int k = 0; k < input.Length; k++)
                     {
                         
                         if (k >= i && k <= j) 
@@ -56,12 +57,14 @@ namespace LABB1
                     {
                         subsAdd += result;
                     }
+
+                    break;
                 }
                 
             }
             Console.WriteLine("\nSumman av substrings: " + subsAdd);
             Console.ReadLine();
-        } // ---------------------------------------------------------------------------------------------------------MAIN
+        } 
     }
 }
 
